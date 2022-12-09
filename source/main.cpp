05433172b56e1e573338b446498861e500e2241d
SpikeHD <spikegdofficial@gmail.com>
2022-12-08 22:46:58 -0800

    working demo

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,29 @@
 #include <nds.h>
 #include <stdio.h>
 
+volatile int frame = 0;
+
+void Vblank() {
+	frame++;
+}
+
 int main(void) {
 	iprintf("This is a test!");
+
+	consoleDemoInit();
+
+	// Store touch position
+	touchPosition touchXY;
+	irqSet(IRQ_VBLANK, Vblank);
+
+	while(true) {
+		swiWaitForVBlank();
+
+		touchRead(&touchXY);
+
+		iprintf("Touch X: %04X\n", touchXY.rawx);
+		iprintf("Touch Y: %04X\n", touchXY.rawy);
+		
+		iprintf("Frame: %d\n", frame);
+	}
 }
